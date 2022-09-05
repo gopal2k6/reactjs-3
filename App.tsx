99dc317fbe7559690c3bd32de2d9886fa96fab32
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-05 16:49:55 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import * as React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { message: 'This is First Message', subTxt: 'Submit' };
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount()');
+  }
+
+  swithState() {
+    this.setState({ message: 'Thank you 12345', subTxt: 'ThankYou' });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <p>{this.state.message}</p>
+        <a onClick={this.changeState.bind(this)} href="#">
+          CLick Here
+        </a>
+      </div>
+    );
+  }
 }
